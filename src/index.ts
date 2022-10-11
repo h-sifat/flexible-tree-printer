ae1e9b3c6cf1b20bbcec5066636c4a8b867e3712
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2022-10-12 00:04:16 +0600

    feat: simplify printTree interface with a wrapper
    The wrapper provides most of the default properties in the printTree
    argument function so that the user doesn't have do it themselves.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-import { Connectors } from "./interface";
+import {
+  forEach,
+  printNode,
+  getSubNodes,
+  validatePrintTreeArgument,
+} from "./util";
+import __printTree__ from "./print";
+import getNodePrefix from "./prefix-generator";
+import type { Connectors, PrintTree_Argument } from "./interface";
 
 export const connectors: Connectors = Object.freeze({
   tee: "├",
@@ -7,3 +15,40 @@ export const connectors: Connectors = Object.freeze({
   space: " ",
   vLine: "│",
 });
+
+export const DEFAULT_ARGUMENT: Omit<
+  PrintTree_Argument,
+  "path" | "xLevel" | "yLevel" | "xLevelsOfLastNodeAncestors"
+> = Object.freeze({
+  forEach,
+  printNode,
+  connectors,
+  getSubNodes,
+  getNodePrefix,
+  parentNode: null,
+  maxLevel: Infinity,
+  sortNodes: () => {},
+  indentationLength: 4,
+  numOfHLinesBeforeNode: 2,
+  shouldDescendIntoSubNode: () => true,
+});
+
+export function printTree(userArgument: Partial<PrintTree_Argument>) {
+  const printTreeArgument: PrintTree_Argument = {
+    ...DEFAULT_ARGUMENT,
+    // overriding default arguments
+    ...userArgument,
+
+    // properties that the user is not allowed to change
+    path: [],
+    xLevel: 1,
+    yLevel: 1,
+    xLevelsOfLastNodeAncestors: [],
+  };
+
+  validatePrintTreeArgument(printTreeArgument);
+
+  console.log(".");
+
+  __printTree__(printTreeArgument);
+}
